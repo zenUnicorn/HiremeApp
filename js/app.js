a570c54165ed678fbe78aa730d3dcca6c2b3b87b
shittu olumide <40125098+zenUnicorn@users.noreply.github.com>
2020-02-05 10:55:36 +0100

    updated js file

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ contract Hireme =
     picture : string,
     voteCount : int }
     
+    
  record state = { 
     details : map(int, detail),
     detailsNum : int}
@@ -36,10 +37,8 @@ contract Hireme =
   let updatedDetails = state.details{ [index].voteCount = updatedvoteCount }
   put(state{ details = updatedDetails })
 `;
-const contractAddress ='ct_41pSis1ZFvxPWUkw6j6u7AG1k9FaepuaaQwRJKP6S9oXcZxYT';
+const contractAddress = 'ct_41pSis1ZFvxPWUkw6j6u7AG1k9FaepuaaQwRJKP6S9oXcZxYT';
 var client = null;
-var jobArray = [];
-var jobLength = 0;
 
 //user-defined function
 function olu() {
@@ -47,94 +46,106 @@ function olu() {
 }
 
 function renderJobs() {
-    jobArray = jobArray.sort(function(a,b){return b.votes-a.votes})
-    var template = $('#template').html();
-    Mustache.parse(template);
-    var rendered = Mustache.render(template, {jobArray});
+  var jobArray = JSON.parse(localStorage.JobDb);
 
-    $('#jobBody').html(rendered);
-    console.log("Rendered")
-  }
+  // jobArray = jobArray.sort(function(a,b){return b.votes-a.votes})
+  var template = $('#template').html();
+  Mustache.parse(template);
+  var rendered = Mustache.render(template, {
+    jobArray
+  });
 
-   async function callStatic(func, args){
-      const contract = await client.getContractInstance(contractSource, {contractAddress});
-      const calledGet = await contract.call(func, args, {callStatic: true}).catch(e => console.error(e));
-      
+  $('#jobBody').html(rendered);
+  console.log("Rendered")
+}
 
-      const decodedGet = await calledGet.decode().catch(e => console.error(e));
-      return decodedGet;
-   }
+async function callStatic(func, args) {
+  const contract = await client.getContractInstance(contractSource, {
+    contractAddress
+  });
+  const calledGet = await contract.call(func, args, {
+    callStatic: true
+  }).catch(e => console.error(e));
 
-async function ContractCall(func, args, value){
-  const contract = await client.getContractInstance(contractSource, {contractAddress});
-  const calledSet = await contract.call(func, args, {amount: value}).catch(e => console.error(e));
+
+  const decodedGet = await calledGet.decode().catch(e => console.error(e));
+  return decodedGet;
+}
+
+async function ContractCall(func, args, value) {
+  const contract = await client.getContractInstance(contractSource, {
+    contractAddress
+  });
+  const calledSet = await contract.call(func, args, {
+    amount: value
+  }).catch(e => console.error(e));
 
   return calledSet;
 }
 
-   
-  window.addEventListener('load', async () => { 
-    $("#loader").show();
-  
-    client = await Ae.Aepp();
-  
-    detailsNum = await callStatic ('getdetailsNum',[]);
-    for (let i = 1;  i<= detailsNum; i++) {
-          const job = await callStatic('getJob',[i]);
 
-          jobArray.push({
-            companyName: job.companyName,
-            jobRole: job.jobRole,
-            jobLink: job.jobLink,
-            jobLocation: job.jobLocation,
-            picture: job.picture,
-            index: i,
-            votes: job.voteCount,
-          })
-      
-    }
-  
-    renderJobs();
-  
-    $("#loader").hide();
-  });
-  
-jQuery("#jobBody").on("click", ".voteBtn", async function(event){
+window.addEventListener('load', async () => {
   $("#loader").show();
 
-    const value = $(this).siblings('input').val();
-    const dataIndex = event.target.id;
+  client = await Ae.Aepp();
 
-    await contractCall('voteJob', [dataIndex], value);
+  detailsNum = await callStatic('getdetailsNum', []);
+  for (let i = 1; i <= detailsNum; i++) {
+    const job = await callStatic('getJob', [i]);
 
-    const foundIndex = jobArray.findIndex(job => job.index == dataIndex);
-    jobArray[foundIndex].votes += parseInt(value, 10);
-
-    renderJobs();
-    $("#loader").hide();
-  });
-  
-  $('#registerBtn').click(async function(){
-    $("#loader").show();
-
-    const company = ($('#companyUrl').val()),
-        role = ($('#roleUrl').val()),
-        picture = ($('#pictureUrl').val()),
-        job = ($('#jobUrl').val()),
-        location = ($('#loactionUrl').val());
-
-    await contractCall('addJob', [company, role, picture, job, location], 0);
-  
     jobArray.push({
-      companyName: company,
-      jobRole: role,
-      jobLink: job,
-      jobLocation: location,
-      picture: picture,
-      index: jobArray.length+1,
-      votes: 0
+      companyName: job.companyName,
+      jobRole: job.jobRole,
+      jobLink: job.jobLink,
+      jobLocation: job.jobLocation,
+      picture: job.picture,
+      index: i,
+      votes: job.voteCount,
     })
-  
-    renderJobs();
-    $("#loader").hide();
-  });
+
+  }
+
+  renderJobs();
+
+  $("#loader").hide();
+});
+
+jQuery("#jobBody").on("click", ".voteBtn", async function (event) {
+  $("#loader").show();
+
+  const value = $(this).siblings('input').val();
+  const dataIndex = event.target.id;
+
+  await contractCall('voteJob', [dataIndex], value);
+
+  const foundIndex = jobArray.findIndex(job => job.index == dataIndex);
+  jobArray[foundIndex].votes += parseInt(value, 10);
+
+  renderJobs();
+  $("#loader").hide();
+});
+
+$('#registerBtn').click(async function () {
+  $("#loader").show();
+
+  const company = ($('#companyUrl').val()),
+    role = ($('#roleUrl').val()),
+    picture = ($('#pictureUrl').val()),
+    job = ($('#jobUrl').val()),
+    location = ($('#loactionUrl').val());
+
+  await contractCall('addJob', [company, role, picture, job, location], 0);
+
+  jobArray.push({
+    companyName: company,
+    jobRole: role,
+    jobLink: job,
+    jobLocation: location,
+    picture: picture,
+    index: jobArray.length + 1,
+    votes: 0
+  })
+
+  renderJobs();
+  $("#loader").hide();
+});
